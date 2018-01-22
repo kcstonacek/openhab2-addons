47fa5a1112d65be623848551871aced407a067da
KC Stonacek <source@stonacek.nz>
2018-01-22 16:18:20 +1300

    Aligned HS110 v2 emeter calculations to v1.
    fixed HS110 v2 emeter calculations (i.e. milliwatt to watt)
    so they are consistent with HS110 v1 readings
    
    Signed-off-by: KC Stonacek <source@stonacek.nz> (github: kcstonacek)

--- addons/binding/org.openhab.binding.tplinksmarthome/src/main/java/org/openhab/binding/tplinksmarthome/internal/model/Realtime.java
+++ addons/binding/org.openhab.binding.tplinksmarthome/src/main/java/org/openhab/binding/tplinksmarthome/internal/model/Realtime.java
@@ -8,8 +8,6 @@
  */
 package org.openhab.binding.tplinksmarthome.internal.model;
 
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Data class for reading tp-Link Smart Plug energy monitoring.
  * Only getter methods as the values are set by gson based on the retrieved json.
@@ -18,37 +16,57 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Realtime extends ErrorResponse {
 
-    /* 
-     * for devices like:
-     *   {"system":{"get_sysinfo":{"sw_ver":"1.0.10 Build 161012 Rel.165510","hw_ver":"2.0","type":"IOT.SMARTPLUGSWITCH","model":"HS110(AU)"
-     * The get_realtime json response looks like:
-     *   "emeter":{"get_realtime":{"voltage_mv":239425,"current_ma":1234,"power_mw":123456,"total_wh":12345,"err_code":0}}}
-     *
-     * use gson's alternate {@link com.google.gson.annotations.SerializedName} so both are valid
-     */
-
-    @SerializedName(value="current", alternate={"current_ma"})
     private double current;
-    @SerializedName(value="power", alternate={"power_mw"})
     private double power;
-    @SerializedName(value="total", alternate={"total_wh"})
     private double total;
-    @SerializedName(value="voltage", alternate={"voltage_mv"})
     private double voltage;
 
+    /*
+     * for v2 devices with json like:
+     * {"system":{"get_sysinfo":{"sw_ver":"1.0.10 Build 161012 Rel.165510","hw_ver":"2.0","type":"IOT.SMARTPLUGSWITCH",
+     * "model":"HS110(AU)"
+     * The get_realtime json response looks like:
+     * "emeter":{"get_realtime":{"voltage_mv":239425,"current_ma":1234,"power_mw":123456,"total_wh":12345,"err_code":0}}
+     * }
+     *
+     * it appears the v1 uses float values for volts, amps, watts, and kwh,
+     * where as v2 uses long values for mv, ma ,mw, wh.
+     * v2 adjustments are made in the getter methods.
+     */
+
+    private double current_ma;
+    private double power_mw;
+    private double total_wh;
+    private double voltage_mv;
+
     public double getCurrent() {
+        if (current == 0 && current_ma != 0) {
+            return current_ma / 1000; // v1 displays as amps
+        }
+
         return current;
     }
 
     public double getPower() {
+        if (power == 0.0 && power_mw != 0.0) {
+            return power_mw / 1000; // v1 displays as watts
+        }
+
         return power;
     }
 
     public double getTotal() {
+        if (total == 0.0 && total_wh != 0.0) {
+            return total_wh / 1000; // v1 displays as kWh
+        }
+
         return total;
     }
 
     public double getVoltage() {
+        if (total == 0.0 && voltage_mv != 0.0) {
+            return voltage_mv / 1000; // v1 displays as volts
+        }
         return voltage;
     }
 
